2590357ee6c24edc0f5c0c8e51d47aae1e781a5d
juniorhans <afwfw17@gmail.com>
2026-04-02 00:52:21 -0300

    carrossel

--- js/main.js
+++ js/main.js
@@ -22,3 +22,17 @@ caseCards.forEach(card => {
     }, 10);
   });
 });
+
+const slides = document.querySelectorAll(".hero-slide");
+
+let currentSlide = 0;
+
+function changeSlide() {
+  slides[currentSlide].classList.remove("active");
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(changeSlide, 4000);
